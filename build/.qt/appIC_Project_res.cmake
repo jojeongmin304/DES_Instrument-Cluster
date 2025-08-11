35faa799216cc01ac25d49defe02106431bf8d46
smwkbgmn <6moguai9@gmail.com>
2025-08-11 14:16:52 +0200

    reorder files

--- build/.qt/appIC_Project_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appIC_Project")
-set(working_dir "/Users/donghyun/All/seame/des_ic/jjo")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/Users/donghyun/All/seame/des_ic/build/.qt/appIC_Project_res.txt")
